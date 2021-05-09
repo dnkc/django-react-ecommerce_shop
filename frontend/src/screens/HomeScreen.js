1dab23848b3c2cfd663c2f5ab05a3ca75172cc8f
dnkc <dnkcdesign@gmail.com>
2021-05-08 22:28:49 -0400

    added backend api

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
-import React from "react";
-import products from "../products";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const { data } = await axios.get("/api/products/");
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    }
+    fetchProduct();
+  }, []);
+
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
@@ -57,7 +66,7 @@ const ProductScreen = ({ match }) => {
               <ListGroup.Item>
                 <Button
                   className="btn btn-block"
-                  disabled={product.countInStock == 0}
+                  disabled={product.countInStock === 0}
                   type="button"
                 >
                   Add To Cart
